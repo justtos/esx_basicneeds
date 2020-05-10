a8a0aff8567e7bc5eeea4f4c0ec2eb75ebf8b682
justtos <atila_pan@abv.bg>
2020-05-10 11:13:03 +0300

    Add PoCs
    + Add a few proof of concept items 
    + Add different queries for different ESX versions

--- esx_basicneeds.sql
+++ esx_basicneeds.sql
@@ -1,6 +1,36 @@
+-- For ESX versions with only weight
+
 USE `es_extended`;
 
 INSERT INTO `items` (`name`, `label`, `weight`) VALUES
-	('bread', 'Pain', 1),
-	('water', 'Eau', 1)
+	('bread', 'Bread', 1),
+	('burger', 'Burger', 1),
+	('fries', 'Fries', 1),
+	('cocacola', 'Coca-Cola', 1),
+	('water', 'Water', 1),
 ;
+
+-- For ESX versions with weight and limit
+
+USE `es_extended`;
+
+INSERT INTO `items` (`name`, `label`, `weight`, `limit` ) VALUES
+	('bread', 'Bread', 1, 20),
+	('burger', 'Burger', 1, 5),
+	('fries', 'Fries', 1, 5),
+	('cocacola', 'Coca-Cola', 1, 10),
+	('water', 'Water', 1, 20),
+;
+
+-- For ESX versions with only weight
+
+USE `es_extended`;
+
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+	('bread', 'Bread', 20),
+	('burger', 'Burger', 5),
+	('fries', 'Fries', 5),
+	('cocacola', 'Coca-Cola', 10),
+	('water', 'Water', 20),
+;
+
